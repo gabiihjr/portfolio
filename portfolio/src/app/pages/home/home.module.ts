f7635249a6162a5ee709daf63b4353276ca1cfc2
Gabriela Hermenegildo <gabriela.hermenegildo@gmail.com>
2023-08-09 15:59:31 -0300

    fix: style in landing page

--- portfolio/src/app/pages/home/home.module.ts
+++ portfolio/src/app/pages/home/home.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { ContactModule } from '../contact/contact.module';
+import { AboutMeModule } from '../about-me/about-me.module';
+import { ProjectsModule } from '../projects/projects.module';
 
 
 @NgModule({
@@ -9,6 +12,9 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
+    ContactModule,
+    AboutMeModule,
+    ProjectsModule
   ],
   exports: [
     HomeComponent,
